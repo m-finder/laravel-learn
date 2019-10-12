3e2d49eed0eb3d329556ee8323934e8a9946be52
wuyunfang <wuyunfang@minzhetech.com>
2019-10-12 16:11:52 +0800

    update

--- routes/websocket.php
+++ routes/websocket.php
@@ -12,13 +12,14 @@ use SwooleTW\Http\Websocket\Facades\Websocket;
 | Here is where you can register websocket events for your application.
 |
 */
-
 Websocket::on('connect', function ($websocket, Request $request) {
     // called while socket on connect
+    $websocket->emit('message', 'welcome');
 });
 
 Websocket::on('disconnect', function ($websocket) {
     // called while socket on disconnect
+    $websocket->emit('message', 'server is gone, bye~');
 });
 
 Websocket::on('example', function ($websocket, $data) {
